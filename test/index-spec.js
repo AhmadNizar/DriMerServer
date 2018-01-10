3d93a3cefe3fe393d059acc6230acc10fabd9ff8
AhmadNizar <ahmadnizar.owl@gmail.com>
2018-01-10 16:55:14 +0700

    -test

--- test/index-spec.js
+++ test/index-spec.js
@@ -2,18 +2,17 @@ var chai = require('chai')
 var chaitHttp = require('chai-http')
 var server = require('../app')
 var should = chai.should()
-var expect = chai.expect
-var assert = require('assert')
 
 chai.use(chaitHttp)
 
-describe('get run', function () {
-  it('/', function(done){
+describe('App', function () {
+  it('send back object', function(done){
     chai.request(server)
     .get('/')
-    .end(function(err,res){
+    .end((err,res) => {
       res.body.should.be.an('object')
       done()
+      server.close()
     })
   })
 })
