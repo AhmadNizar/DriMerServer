141fed45306a7eba667b208c95d6e449b7ef34fc
AhmadNizar <ahmadnizar.owl@gmail.com>
2018-01-10 16:10:05 +0700

    -test travis

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ app.use('/', (req, res) => {
   res.send({jos : 'run' })
 })
 
+app.use('/test', (req, res) => {
+  res.send({Nijar : 'tamvan'})
+})
+
 app.listen((process.env.PORT || '3000'), () => {
 	console.log('jalan port 3000 tong');
 })
