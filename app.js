17fdc6ac627da6d81f101a8d874d59a0cb752633
AhmadNizar <ahmadnizar.owl@gmail.com>
2018-01-10 19:55:38 +0700

    -test2

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const app = require('express')()
 
-app.use('/', (req, res) => {
-  res.send({jos : 'run' })
-})
-
 app.use('/test', (req, res) => {
   res.send({Nijar : 'tamvan'})
+})
+
+app.use('/', (req, res) => {
+  res.send({jos : 'run' })
 })
 
 app.listen((process.env.PORT || '3000'), () => {
